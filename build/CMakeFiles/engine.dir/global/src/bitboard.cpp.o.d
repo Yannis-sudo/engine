80429b131d1d06c8a6d7f0a627baaf3a584357f4
Yannis-sudo <ya10be@gmx.de>
2026-01-17 16:29:48 +0100

    Removed build and .vscode 2nd try

--- build/CMakeFiles/engine.dir/global/src/bitboard.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/engine.dir/global/src/bitboard.cpp.o: \
- /home/pi/dev/engine/engine_1/global/src/bitboard.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/engine.dir/src/engine.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/engine.dir/src/engine.cpp.o: \
- /home/pi/dev/engine/engine_1/src/engine.cpp /usr/include/stdc-predef.h
